02c3ace947ea96c9c746d1542f43cf534f3b7769
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2019-12-01 12:01:48 -0500

    finetuning script

---
+++ run.ps1
@@ -0,0 +1,9 @@
+python .\transformers\examples\run_lm_finetuning.py `
+    --output_dir=output `
+    --model_type=roberta `
+    --model_name_or_path=roberta-base `
+    --do_train `
+    --train_data_file="C:/Users/dhruv/Documents/NLP/project/data/train.txt" `
+    --do_eval `
+    --eval_data_file="C:/Users/dhruv/Documents/NLP/project/data/test.txt" `
+    --mlm `
